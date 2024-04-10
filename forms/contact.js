8f4fce11e17c5f1303c3f8719dc33e101a943a10
Hugodujour <hugogreg@live.fr>
2024-04-10 15:08:54 +0200

    maj

--- forms/contact.js
+++ forms/contact.js
@@ -1,13 +1,13 @@
 // Replace contact@example.com with your real receiving email address
-const receiving_email_address = 'hugogreg@live.fr';
+const receiving_email_address = "contact@consonance-bordeaux.fr";
 
 const contact = new PHP_Email_Form();
 contact.ajax = true;
 
 contact.to = receiving_email_address;
-contact.from_name = document.getElementById('name').value;
-contact.from_email = document.getElementById('email').value;
-contact.subject = document.getElementById('subject').value;
+contact.from_name = document.getElementById("name").value;
+contact.from_email = document.getElementById("email").value;
+contact.subject = document.getElementById("subject").value;
 
 // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
 /*
@@ -19,8 +19,8 @@ contact.smtp = {
 };
 */
 
-contact.add_message(document.getElementById('name').value, 'From');
-contact.add_message(document.getElementById('email').value, 'Email');
-contact.add_message(document.getElementById('message').value, 'Message', 10);
+contact.add_message(document.getElementById("name").value, "From");
+contact.add_message(document.getElementById("email").value, "Email");
+contact.add_message(document.getElementById("message").value, "Message", 10);
 
 console.log(contact.send());
